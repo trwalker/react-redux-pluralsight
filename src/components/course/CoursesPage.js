811303f30f85ad32527c13a3cd164808c233cbae
Tim Walker <trwalker@godaddy.com>
2016-10-21 06:13:19 -0700

    CoursesPage cleanup

--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -8,42 +8,15 @@ import * as courseActions from '../../actions/courseActions';
 class CoursesPage extends React.Component {
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            course: { title: '' }
-        };
-
-        this.onTitleChange = this.onTitleChange.bind(this);
-        this.onClickSave = this.onClickSave.bind(this);
-    }
-
-    onTitleChange(event) {
-        let course = this.state.course;
-        course.title = event.target.value;
-        this.setState({ course: course });
-    }
-
-    onClickSave() {
-        this.props.actions.createCourse(this.state.course);
-    }
-
-    courseRow(course, index) {
-        return <div key={index}>{course.title}</div>;
     }
 
     render() {
+        const {courses} = this.props;
+
         return (
             <div>
                 <h1>Courses</h1>
-                <CourseList courses={this.props.courses}/>
-                <h3>Add Course</h3>
-                <div className="form-group">
-                    <label>Title</label>
-                    <input id="courseTitle" className="form-control" type="text" onChange={this.onTitleChange} value={this.state.course.title} />
-                </div>
-                <div className="form-group">
-                    <input className="btn btn-primary btn-large" type="submit" value="Save" onClick={this.onClickSave} />
-                </div>
+                <CourseList courses={courses} />
             </div>
         );
     }
